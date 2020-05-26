d964fd999a2e0eaa378715423ce00db9242ea887
BarbaraGajewska <53317723+BarbaraGajewska@users.noreply.github.com>
2020-05-26 14:47:35 +0200

    Update environment.py

--- tests/environment.py
+++ tests/environment.py
@@ -1,8 +1,5 @@
 from tests.framework.selenium_driver import webapp
-#import allure_behave.hooks
-
 
 def after_feature(context, scenario):
     webapp.close_app()
 
-#allure_report("/Users/basia/PycharmProjects/PyPomBDD/tests")
